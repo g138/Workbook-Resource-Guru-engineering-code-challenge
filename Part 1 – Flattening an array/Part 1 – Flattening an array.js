62c0cac3ca6325762b9974ddfecb0ccf7ae89a72
Gaurav Gupta <36992519+g138@users.noreply.github.com>
2022-07-29 14:07:01 +0100

    v.0.3 (part_1)

---
+++ Part 1 – Flattening an array/Part 1 – Flattening an array.js
@@ -0,0 +1,7 @@
+// Flattening of array using the JavaScript in built flat() method by passing Depth as Infinuty
+
+let inputArray = [1, [2, [3]], 4];
+
+let flattenArray = inputArray.flat(Infinity);
+
+console.log(flattenArray);
